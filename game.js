b1307b9d46d6f08232f7f524d6053812fad2717d
Matheus Tavares <matheus.tavares45@gmail.com>
2021-09-05 03:59:37 -0300

    adding more sprites and background movement

--- game.js
+++ game.js
@@ -10,8 +10,9 @@ kaboom({
 });
 
 // load assets
-loadSprite("galaxy", "/assets/sprites/galaxy.jpg");
-loadSprite("nav", "/assets/sprites/nav-spritesheet-10x10.png", {
+loadSprite("galaxy", "/assets/sprites/backgrounds/galaxy.jpg");
+loadSprite("galaxy2", "/assets/sprites/backgrounds/galaxy2.jpg");
+loadSprite("nav", "/assets/sprites/players/nav-no-fire-spritesheet-10x10.png", {
   sliceX: 10,
   sliceY: 10,
   anims: {
@@ -41,7 +42,7 @@ loadSprite("nav", "/assets/sprites/nav-spritesheet-10x10.png", {
     },
   },
 });
-loadSprite("bullet", "/assets/sprites/bullet-spritesheet-5x5.png", {
+loadSprite("bullet", "/assets/sprites/bullets/blue-spritesheet-5x5.png", {
   sliceX: 5,
   sliceY: 5,
   anims: {
@@ -52,32 +53,70 @@ loadSprite("bullet", "/assets/sprites/bullet-spritesheet-5x5.png", {
   },
 });
 
-// defining a scene
 scene("game", () => {
-	// define layers and the default layer
 	layers([
+    "bg",
 		"game",
 		"ui",
 	], "game");
 
-	// background
 	add([
 		sprite("galaxy", { width: width(), height: height(), }),
+    'bg',
+    pos(0, 0),
+    layer("bg"),
 	]);
 
-	// player
+  add([
+    sprite("galaxy2", { width: width(), height: height(), }),
+    'bg',
+    'bgaux',
+    pos(0, -height()),
+    layer("bg"),
+  ]);
+
+  action("bg", (b) => {
+    b.move(0, 1);
+    if (b.pos.y > height()) {
+      b.destroy();
+      if (b.is('bgaux')) {
+        add([
+          sprite("galaxy2", { width: width(), height: height(), }),
+          'bg',
+          'bgaux',
+          pos(0, -height()),
+          layer("bg"),
+        ]);
+      } else {
+        add([
+          sprite("galaxy", { width: width(), height: height(), }),
+          'bg',
+          pos(0, -height()),
+          layer("bg"),
+        ]);
+      }
+    }
+  });
+  
+  const MOVE_SPEED = 200;
+  const BULLET_SPEED = 400;
+  let PLAYER_SHOOT_SPEED = 0.2;
+  const PLAYER_SCALE_ONE = [2, 11, 0.8];
+  const PLAYER_SCALE_TWO = [3, 17, 1];
+
+  // PLAYER
+
 	const player = add([
 		sprite("nav"),
 		pos(center()),
-    scale(3),
+    scale(PLAYER_SCALE_ONE[0]),
 		area(),
+    "player",
 	]);
 
-  const MOVE_SPEED = 200;
-  const BULLET_SPEED = 400;
-  let PLAYER_SHOOT_SPEED = 0.15;
-
   player.play("idle");
+
+  // PLAYER MOVEMENT
 
   keyDown("up", () => {
     player.move(0, -MOVE_SPEED)
@@ -155,8 +194,8 @@ scene("game", () => {
     if (keyIsDown('x')) {
       const bullet = add([
         sprite("bullet"),
-        pos(player.pos.x + 17, player.pos.y - 2),
-        scale(1),
+        pos(player.pos.x + PLAYER_SCALE_ONE[1], player.pos.y - 2),
+        scale(PLAYER_SCALE_ONE[2]),
         "bullet"
       ]);
 
@@ -174,6 +213,7 @@ scene("game", () => {
     }
 	});
 });
+
 
 scene("lose", (score) => {
 	add([
