00eaf2c04a04810d6d5cff12518ff1ce3c48b260
Matheus Tavares <matheus.tavares45@gmail.com>
2021-09-11 14:07:32 -0300

    add languages file

---
+++ languages.js
@@ -0,0 +1,13 @@
+export const pt = {
+  "tutorial": "Use as setas para navegar e X para atirar",
+  "title": "Project Galaxy",
+  "subtitle": "Aperte Espaco para comecar"
+}
+
+export const en = {
+  "tutorial": "Use directional keys to navigate and X to shoot",
+  "title": "Project Galaxy",
+  "subtitle": "Press Space to start"
+}
+
+export default { pt, en }
