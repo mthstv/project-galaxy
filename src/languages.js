3cb335987adc699498bb245cbfa5b9f2cf3dc7e2
MatheusT45 <matheus.tavares45@gmail.com>
2021-09-12 19:40:09 -0300

    add name input and personal best labels on languages

--- src/languages.js
+++ src/languages.js
@@ -1,13 +1,17 @@
 export const pt = {
   "tutorial": "Use as setas para navegar e X para atirar",
   "title": "Project Galaxy",
-  "subtitle": "Aperte Espaco para comecar"
+  "subtitle": "Aperte Espaco para comecar",
+  "nameInput": "Seu nome: ",
+  "personalBest": "Melhor pontuacao: "
 }
 
 export const en = {
   "tutorial": "Use directional keys to navigate and X to shoot",
   "title": "Project Galaxy",
-  "subtitle": "Press Space to start"
+  "subtitle": "Press Space to start",
+  "nameInput": "Your name: ",
+  "personalBest": "Personal Best: "
 }
 
 export async function loadLanguage(browserLanguage) {
