908020cbc48ea210d65b337e7bdf3279fd532b81
MatheusT45 <matheus.tavares45@gmail.com>
2021-09-14 20:16:37 -0300

    call special upgrade on full meter

--- src/logic/special.js
+++ src/logic/special.js
@@ -1,9 +1,11 @@
+import loadSpecialUpgrade from "./special-upgrade.js";
+
 export default function loadPlayerSpecialMeter(specialLimit, currentSpecial) {
-  for (let n = 0; n < specialLimit; n++) {
-    if (n < currentSpecial) {
+  for (let n = 1; n <= specialLimit; n++) {
+    if (n <= currentSpecial) {
       add([
         sprite("special-point-empty"),
-        pos(n * 22, height() - 40),
+        pos((n - 1) * 22, height() - 40),
         scale(2),
         layer("ui"),
         origin("botleft"),
@@ -13,11 +15,20 @@ export default function loadPlayerSpecialMeter(specialLimit, currentSpecial) {
 
     add([
       sprite("special-point-filled"),
-      pos(n * 22, height() - 40),
+      pos((n - 1) * 22, height() - 40),
       scale(2),
       layer("game"),
       origin("botleft"),
       "sp"
     ]);
+  }
+  if (specialLimit <= currentSpecial) {
+    destroyAll("sp");
+    loadPlayerSpecialMeter(specialLimit, 0);
+
+    const player = get("player")[0];
+    player.special = 0;
+
+    loadSpecialUpgrade(player);
   }
 }
