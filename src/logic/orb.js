523e1e4227c20aad0ebccc7d27c0a53855b926ab
MatheusT45 <matheus.tavares45@gmail.com>
2022-02-22 19:51:17 -0300

    add more rectangles to the trail

--- src/logic/orb.js
+++ src/logic/orb.js
@@ -3,7 +3,7 @@ export default function loadOrb(originPos) {
   const orb = add([
     sprite("orb"),
     pos(originPos),
-    scale(1),
+    scale(1.2),
     area({ scale: 0.8 }),
     origin("center"),
     "orb",
@@ -13,44 +13,66 @@ export default function loadOrb(originPos) {
 
   const rectangle1 = add([
     pos(originPos.x, originPos.y - 10),
-    rect(20, 8),
+    rect(30, 16),
     rotate(135),
     origin("center"),
     color(0, 229, 255),
-    opacity(0.5),
+    opacity(0.2),
     "trail",
     area(),
   ]);
 
   const rectangle2 = add([
     pos(originPos.x, originPos.y - 25),
-    rect(16, 7),
+    rect(26, 12),
     rotate(135),
     origin("center"),
     color(0, 229, 255),
-    opacity(0.5),
+    opacity(0.2),
     "trail",
     area(),
   ]);
 
   const rectangle3 = add([
     pos(originPos.x, originPos.y - 40),
-    rect(14, 6),
+    rect(24, 10),
     rotate(135),
     origin("center"),
     color(0, 229, 255),
-    opacity(0.5),
+    opacity(0.2),
     "trail",
     area(),
   ]);
 
-  const rectangle4= add([
+  const rectangle4 = add([
     pos(originPos.x, originPos.y - 50),
-    rect(12, 5),
+    rect(22, 8),
     rotate(135),
     origin("center"),
     color(0, 229, 255),
-    opacity(0.5),
+    opacity(0.2),
+    "trail",
+    area(),
+  ]);
+
+  const rectangle5 = add([
+    pos(originPos.x, originPos.y - 60),
+    rect(18, 6),
+    rotate(135),
+    origin("center"),
+    color(0, 229, 255),
+    opacity(0.2),
+    "trail",
+    area(),
+  ]);
+
+  const rectangle6 = add([
+    pos(originPos.x, originPos.y - 70),
+    rect(15, 4),
+    rotate(135),
+    origin("center"),
+    color(0, 229, 255),
+    opacity(0.2),
     "trail",
     area(),
   ]);
@@ -61,10 +83,14 @@ export default function loadOrb(originPos) {
     rectangle2.angle = rectangle2.pos.angle(player.pos);
     rectangle3.angle = rectangle3.pos.angle(player.pos);
     rectangle4.angle = rectangle4.pos.angle(player.pos);
+    rectangle5.angle = rectangle5.pos.angle(player.pos);
+    rectangle6.angle = rectangle6.pos.angle(player.pos);
     orb.moveTo(player.pos.x, player.pos.y, 700);
-    rectangle1.moveTo(player.pos.x, player.pos.y, 700);
-    rectangle2.moveTo(player.pos.x, player.pos.y, 700);
-    rectangle3.moveTo(player.pos.x, player.pos.y, 700);
-    rectangle4.moveTo(player.pos.x, player.pos.y, 700);
+    rectangle1.moveTo(player.pos.x, player.pos.y, 720);
+    rectangle2.moveTo(player.pos.x, player.pos.y, 720);
+    rectangle3.moveTo(player.pos.x, player.pos.y, 720);
+    rectangle4.moveTo(player.pos.x, player.pos.y, 720);
+    rectangle5.moveTo(player.pos.x, player.pos.y, 720);
+    rectangle6.moveTo(player.pos.x, player.pos.y, 720);
   })
 }
